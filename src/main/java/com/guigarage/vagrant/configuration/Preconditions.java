58859562a64b89f46f23808849ec4615361bdc2f
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2013-01-26 03:16:14 +0100

    introducing Preconditions

---
+++ src/main/java/com/guigarage/vagrant/configuration/Preconditions.java
@@ -0,0 +1,22 @@
+package com.guigarage.vagrant.configuration;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Preconditions {
+
+	public static <T> T checkNotNull(T t, String message) {
+		if (t == null) {
+			throw new NullPointerException(message);
+		}
+		return t;
+	}
+
+	public static void checkState(boolean b, String meesage) {
+		if (!b) {
+			throw new IllegalStateException(meesage);
+		}
+	}
+
+}
