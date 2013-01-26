797b754a31420e440442dd479cf8d59d82c0966c
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2013-01-26 13:03:07 +0100

    splitted into two tests

--- src/test/java/com/guigarage/vagrant/configuration/VagrantConfigurationUtilitiesTest.java
+++ src/test/java/com/guigarage/vagrant/configuration/VagrantConfigurationUtilitiesTest.java
@@ -14,27 +14,27 @@ public class VagrantConfigurationUtilitiesTest {
 	}
 
 	@Test
-	public void testConfiguration() {
-		{
-			VagrantEnvironmentConfig config = new VagrantEnvironmentConfig(null);
-			String vagrantFileContent = VagrantConfigurationUtilities
-					.createVagrantFileContent(config);
-			assertTrimEquals("Vagrant::Config.run do |config|" + "\n" + "end",
-					vagrantFileContent);
-		}
-
-		{
-			List<VagrantVmConfig> vmConfigs = new ArrayList<VagrantVmConfig>();
-			vmConfigs.add(new VagrantVmConfig("unitTest", null, null,
-					"lucid32", null, null, null, false));
-			VagrantEnvironmentConfig config = new VagrantEnvironmentConfig(
-					vmConfigs);
-			String vagrantFileContent = VagrantConfigurationUtilities
-					.createVagrantFileContent(config);
-			assertTrimEquals("Vagrant::Config.run do |config|" + "\n"
-					+ "config.vm.define :unitTest do |unitTest_config|" + "\n"
-					+ "unitTest_config.vm.box = \"lucid32\"" + "\n" + "end"
-					+ "\n" + "end", vagrantFileContent);
-		}
+	public void testSimpleConfiguration() {
+		VagrantEnvironmentConfig config = new VagrantEnvironmentConfig(null);
+		String vagrantFileContent = VagrantConfigurationUtilities
+				.createVagrantFileContent(config);
+		assertTrimEquals("Vagrant::Config.run do |config|" + "\n" + "end",
+				vagrantFileContent);
 	}
+
+	@Test
+	public void testExtendedConfiguration() {
+		List<VagrantVmConfig> vmConfigs = new ArrayList<VagrantVmConfig>();
+		vmConfigs.add(new VagrantVmConfig("unitTest", null, null, "lucid32",
+				null, null, null, false));
+		VagrantEnvironmentConfig config = new VagrantEnvironmentConfig(
+				vmConfigs);
+		String vagrantFileContent = VagrantConfigurationUtilities
+				.createVagrantFileContent(config);
+		assertTrimEquals("Vagrant::Config.run do |config|" + "\n"
+				+ "config.vm.define :unitTest do |unitTest_config|" + "\n"
+				+ "unitTest_config.vm.box = \"lucid32\"" + "\n" + "end" + "\n"
+				+ "end", vagrantFileContent);
+	}
+
 }
