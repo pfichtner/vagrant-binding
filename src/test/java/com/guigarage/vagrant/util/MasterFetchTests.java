311259a6811f0f441b9e2164be5a1c74c33ee2eb
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2013-01-28 23:53:03 +0100

    house keeping

--- src/test/java/com/guigarage/vagrant/util/MasterFetchTests.java
+++ src/test/java/com/guigarage/vagrant/util/MasterFetchTests.java
@@ -1,26 +1,24 @@
 package com.guigarage.vagrant.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class MasterFetchTests {
 
 	@Test
-	public void testFetchMasterConfigs() {
-		{
-			String content = VagrantUtils.getInstance()
-					.getLucid32MasterContent();
-			assertNotNull(content);
-			assertEquals(true, !content.isEmpty());
-		}
-
-		{
-			String content = VagrantUtils.getInstance()
-					.getLucid64MasterContent();
-			assertNotNull(content);
-			assertEquals(true, !content.isEmpty());
-		}
-
+	public void testFetchMasterConfig32() {
+		String content = VagrantUtils.getInstance().getLucid32MasterContent();
+		assertNotNull(content);
+		assertFalse(content.isEmpty());
 	}
+
+	@Test
+	public void testFetchMasterConfig64() {
+		String content = VagrantUtils.getInstance().getLucid64MasterContent();
+		assertNotNull(content);
+		assertFalse(content.isEmpty());
+	}
+
 }
