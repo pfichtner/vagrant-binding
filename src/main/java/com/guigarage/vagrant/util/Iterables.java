25d9df40f09fa7e0eca2689f6b0f9b370d87d03e
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2013-02-08 21:56:12 +0100

    getOnlyElement added

--- src/main/java/com/guigarage/vagrant/util/Iterables.java
+++ src/main/java/com/guigarage/vagrant/util/Iterables.java
@@ -1,6 +1,8 @@
 package com.guigarage.vagrant.util;
 
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -14,6 +16,29 @@ public final class Iterables {
 	@Nonnull
 	public static <T> Iterable<T> nullToEmpty(@Nullable Iterable<T> iterable) {
 		return iterable == null ? Collections.<T> emptySet() : iterable;
+	}
+
+	public static <T> T getOnlyElement(Iterable<T> collection) {
+		if (collection instanceof List) {
+			List<T> list = (List<T>) collection;
+			if (list.size() != 1) {
+				throw createNotExactlyOneEx(collection);
+			}
+			return list.get(0);
+		} else {
+			Iterator<T> iterator = collection.iterator();
+			T first = iterator.next();
+			if (iterator.hasNext()) {
+				throw createNotExactlyOneEx(collection);
+			}
+			return first;
+		}
+	}
+
+	private static <T> IllegalStateException createNotExactlyOneEx(
+			Iterable<T> collection) {
+		return new IllegalStateException("Collection " + collection
+				+ " contains not exactly one element");
 	}
 
 }
